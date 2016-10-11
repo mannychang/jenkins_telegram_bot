4a3670fb142c97876172eb12e6d58eadf0a28cb5
Manny Chang <mannychang@360.cn>
2016-10-11 15:17:25 +0800

    add config

---
+++ config.py
@@ -0,0 +1,12 @@
+# jenkins config
+jenkins_url = ''
+jenkins_username = ''
+jenkins_token = ''
+
+# telegram config
+telegram_bot_token = ''
+
+# allowed user telegram ids for submitting bot commands
+allowed_users = [
+        '',
+        ]
